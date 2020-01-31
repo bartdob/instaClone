a826565b7b2ada862fbe7a8538eb7127569edf5e
Bartosz Dobrosielski <dobry1@pm.me>
2020-01-31 13:22:47 +0100

    mod of home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,23 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="row">
+        <div class="col-3 p-2">
+            <img style="height: 100px;" src="https://img.favpng.com/10/15/7/stormtrooper-star-wars-the-clone-wars-clone-trooper-png-favpng-WZHpSypu9n5XvJqNQsK2Dz7km_t.jpg" class="rounded-circle">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
         </div>
+        <div class="col-9 pt-2">
+            <div><h1>instaClone</h1></div>
+            <div class="d-flex">
+                <div class="p-2"><strong>151</strong>posts</div>
+                <div class="p-2"><strong>10k</strong>follow</div>
+                <div class="p-2"><strong>212</strong>following</div>
+            </div>
+            
+
+
+        </div>    
+
     </div>
 </div>
 @endsection
