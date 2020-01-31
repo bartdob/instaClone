fc26d02c4ad15791e70fc3950996eab078a86665
Bartosz Dobrosielski <dobry1@pm.me>
2020-01-31 09:12:29 +0100

    add auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    instaClone
                 </div>
 
                 <div class="links">
